9a42b24d3fb7777208d2edb626e725c7a631d42b
Michael Osterlie <michael.osterlie@nexteraanalytics.com>
2021-11-29 14:02:02 -0600

    Use "" for PluginManifest values for test. Added TODO to get tests working

--- src/test/kotlin/KotlinPluginTest.kt
+++ src/test/kotlin/KotlinPluginTest.kt
@@ -5,22 +5,22 @@ class TestClient {
     fun testLoad() {
         val manifest = object : PluginManifest {
             override var id: String
-                get() = TODO("Not yet implemented")
+                get() = ""
                 set(value) {}
             override var name: String
-                get() = TODO("Not yet implemented")
+                get() = ""
                 set(value) {}
             override var author: String
-                get() = TODO("Not yet implemented")
+                get() = ""
                 set(value) {}
             override var version: String
-                get() = TODO("Not yet implemented")
+                get() = ""
                 set(value) {}
             override var minAppVersion: String
-                get() = TODO("Not yet implemented")
+                get() = ""
                 set(value) {}
             override var description: String
-                get() = TODO("Not yet implemented")
+                get() = ""
                 set(value) {}
         }
         val plugin = KotlinPlugin(App(), manifest)
