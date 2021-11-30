b9a2d36f4ba837745cf8e07a358404533ee6ff95
darthmachina <github@outerrim.org>
2021-11-30 00:31:35 -0600

    #3 Implemented builder for Commands and use that for sample Editor and Modal commands

--- src/main/kotlin/KotlinPlugin.kt
+++ src/main/kotlin/KotlinPlugin.kt
@@ -1,3 +1,6 @@
+import builders.CommandBuilder
+import dialogs.KotlinDialog
+
 @OptIn(ExperimentalJsExport::class)
 @JsExport
 @JsName("default")
@@ -6,6 +9,27 @@ class KotlinPlugin(app: App, manifest: PluginManifest) : Plugin(app, manifest) {
 
     override fun onload() {
         loadSettings()
+
+        // This adds a simple command that can be triggered anywhere
+        addCommand(CommandBuilder(
+            "open-sample-modal-simple",
+            "Open sample modal (simple)")
+            .callback {
+                KotlinDialog(app).open()
+            }
+            .build())
+
+        // This adds an editor command that can perform some operation on the current editor instance
+        addCommand(CommandBuilder(
+            "kotlin-editor-command",
+            "Kotlin editor command")
+            .editorCallback { editor, _ ->
+                console.log("Editor selection", editor.getSelection())
+                editor.replaceSelection("Sample Editor Command")
+            }
+            .build())
+
+        // Add Settings tab
         addSettingTab(KotlinPluginSettingsTab(app, this))
         console.log("KotlinPlugin onload()")
     }
